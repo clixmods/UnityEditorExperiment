02c0f791e4558a6997b39d2967373cc09ad19737
clixmods <clix.mods.cod@gmail.com>
2022-09-28 12:50:09 +0200

    Custom Drawer

---
+++ Assets/Scripts/SceneAttributeClass.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+[AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+public class SceneAttribute : PropertyAttribute
+{
+    public int min = 0;
+    public int max = 0;
+    public string oof = "test";
+    
+}
--- Assets/Scripts/StringComponent.cs
+++ Assets/Scripts/StringComponent.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class StringComponent : MonoBehaviour
 {
     [SerializeField] private string _myBeautifulString = "HIPPIE KAI MOTHER FUCKA";
+
+    //[Range()]
+    [SerializeField][Scene] private int test = 0;
     // Start is called before the first frame update
     void Start()
     {
