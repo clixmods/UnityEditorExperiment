0914040e54d288e92328495920f25c84e0a7881c
clixmods <clix.mods.cod@gmail.com>
2022-09-28 10:57:18 +0200

    Project Push

---
+++ Assets/Editor/LogConsole.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class LogConsole : EditorWindow
+{
+    [MenuItem("Tools/Log Console")]
+    void Open()
+    {
+        
+    }
+}
