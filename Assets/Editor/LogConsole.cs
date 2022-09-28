42a3c4708a787d894e272a777d30e245a8612b2d
clixmods <clix.mods.cod@gmail.com>
2022-09-28 11:06:22 +0200

    Exercice 1 done

--- Assets/Editor/LogConsole.cs
+++ Assets/Editor/LogConsole.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,20 @@ using UnityEditor;
 
 public class LogConsole : EditorWindow
 {
+    private string _message;
     [MenuItem("Tools/Log Console")]
-    void Open()
+    static void Open()
     {
-        
+        GetWindow<LogConsole>();
+    }
+
+    private void OnGUI()
+    {
+       _message = EditorGUILayout.TextField(_message);
+       if (GUILayout.Button("Send to console"))
+       {
+           Debug.Log(_message);
+       }
+
     }
 }
