e8a2c97fba8291d4693a452bd67fe579b46b3f03
clixmods <clix.mods.cod@gmail.com>
2022-10-06 12:54:43 +0200

    Commit exo
    Besoin de polish sur les graph mais fonctionnel

---
+++ Assets/Scripts/UI/ScrollbarPlus.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ScrollbarPlus : MonoBehaviour
+{
+    private ScrollRect _scrollRect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _scrollRect = GetComponent<ScrollRect>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0 ; i < _scrollRect.content.childCount ; i++)
+        {
+            Transform child = _scrollRect.content.GetChild(i); //.GetChild(i);
+            if (child == EventSystem.current.currentSelectedGameObject.transform)
+            {
+                float scrollSize = _scrollRect.verticalScrollbar.size;
+                float sizeOfContent = _scrollRect.content.childCount;
+                _scrollRect.verticalScrollbar.value = Mathf.Clamp(( 1 - (i ) / sizeOfContent),0,1);
+            }
+        }
+    }
+}
