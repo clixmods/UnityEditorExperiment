95dbe56280b5f96ae5c1b56ee9ad5b2cdae26e23
clixmods <clix.mods.cod@gmail.com>
2022-11-09 11:50:21 +0100

    Tracer un cercle qui détecte collision avec un point
    En utilisant la classe Gizmos ou Handles et la classe Mathf, créer un script qui dessine un cercle blanc sur la scène et le faire devenir vert quand le point qui sera passé en référence dans ce même script se trouve dans sa surface.
    Vous devez pouvoir changer le rayon du cercle dynamiquement dans l’inspecteur (avec un slider ça serait quand même plus pratique pas vrai ?).
    De cette manière, on détecte la collision entre le cercle et le point.

---
+++ Assets/DrawCircle.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawCircle : MonoBehaviour
+{
+    private const float PI = 3.14f;
+        
+    [Range(1,360)]
+    [SerializeField] private float radius = 2;
+    [Range(2,30)]
+    [SerializeField] private int resolution = 5;
+    [SerializeField] private Transform targetPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnDrawGizmos()
+    {
+        if (targetPoint != null)
+        {
+            Vector3 point = new Vector3(targetPoint.position.x, targetPoint.position.y, 0);    
+            Vector3 pointCircle = new Vector3(transform.position.x, transform.position.y, 0);
+            if ((point - pointCircle).magnitude < radius)
+            {
+                Draw(Color.green);
+            }
+            else
+            {
+                Draw(Color.white);
+            }
+        }
+        else
+        {
+            Draw(Color.white);
+        }
+       
+    }
+    /// <summary>
+    /// Draw a circle 
+    /// </summary>
+    /// <param name="color"> Select a color to draw</param>
+    private void Draw(Color color)
+    {
+        Vector3 ogPosition = transform.position;
+        float step = 2 * PI / resolution;
+        int index = 0;
+        for (float i = 0; i < 2 * PI; i += step) index++;
+
+        Vector3[] points = new Vector3[index];
+        index = 0;
+        for (float i = 0; i < 2 * PI; i += step)
+        {
+            float x = ogPosition.x + (radius * Mathf.Cos(i));
+            float y = ogPosition.y - (radius * Mathf.Sin(i));
+            points[index] = new Vector3(x, y, 0);
+            index++;
+        }
+
+        for (int i = 0; i < points.Length - 1; i++)
+        {
+            Debug.DrawLine(points[i], points[i + 1] , color);
+        }
+
+        Debug.DrawLine(points[^1], points[0] , color);
+    }
+}
