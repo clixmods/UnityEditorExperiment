936f844967fee0d861da64c8179f6c66e1316678
clixmods <clix.mods.cod@gmail.com>
2022-11-30 10:27:10 +0100

    A trier plutard

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityTemplateProjects;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    [SerializeField] private DataPlayer _dataPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
